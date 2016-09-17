4bb6a07889d655f242f935f05e8368ca82e0118b
Oleksii Dykan <alick_bass@mail.ru>
2016-09-17 22:43:52 +0200

    Add dictionary extension tests

--- SwiftyJSONModelTests/DictionaryExtensionTests.swift
+++ SwiftyJSONModelTests/DictionaryExtensionTests.swift
@@ -7,29 +7,18 @@
 //
 
 import XCTest
+@testable import SwiftyJSONModel
 
 class DictionaryExtensionTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testValueForKey() {
+        let key = "testKey"
+        let value = "test"
+        
+        XCTAssertThrowsError(try [:].value(for: key), "If there is no element throw exeption") { error in
+            XCTAssertEqual(error as? JSONModelError, .elementAbsent)
         }
+        XCTAssertEqual(try? [key: value].value(for: key), value)
     }
     
 }
