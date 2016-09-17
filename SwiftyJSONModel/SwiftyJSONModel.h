b995c3b1b5741ae504f2678d12096a3b858ab9ee
Oleksii Dykan <alick_bass@mail.ru>
2016-09-17 15:43:53 +0200

    Initial Commit

---
+++ SwiftyJSONModel/SwiftyJSONModel.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyJSONModel.h
+//  SwiftyJSONModel
+//
+//  Created by Oleksii on 17/09/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyJSONModel.
+FOUNDATION_EXPORT double SwiftyJSONModelVersionNumber;
+
+//! Project version string for SwiftyJSONModel.
+FOUNDATION_EXPORT const unsigned char SwiftyJSONModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyJSONModel/PublicHeader.h>
+
+
