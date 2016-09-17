aba13e85ff617e256713ac6449ab172e7e667298
Oleksii Dykan <alick_bass@mail.ru>
2016-09-17 21:11:44 +0200

    Add json extension with throwable methods

--- SwiftyJSONModel/JSONExtension.swift
+++ SwiftyJSONModel/JSONExtension.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+public extension JSON {
+    public func boolValue() throws -> Bool {
+        guard let boolValue = bool else {
+            throw JSONModelError.elementAbsent
+        }
+        return boolValue
+    }
+    
+    public func intValue() throws -> Int {
+        guard let intValue = int else {
+            throw JSONModelError.elementAbsent
+        }
+        return intValue
+    }
+    
+    public func doubleValue() throws -> Double {
+        guard let doubleValue = double else {
+            throw JSONModelError.elementAbsent
+        }
+        return doubleValue
+    }
+    
+    public func stringValue() throws -> String {
+        guard let stringValue = string else {
+            throw JSONModelError.elementAbsent
+        }
+        return stringValue
+    }
+    
+    public func arrayValue() throws -> [JSON] {
+        guard let arrayValue = array else {
+            throw JSONModelError.elementAbsent
+        }
+        return arrayValue
+    }
+    
+    public func uuidStringValue() throws -> String {
+        guard let stringValue = string else {
+            throw JSONModelError.elementAbsent
+        }
+        guard let uuidStrigValue = NSUUID(uuidString: stringValue) else {
+            throw JSONModelError.invalidUUIDString
+        }
+        
+        return uuidStrigValue.uuidString
+    }
+}
