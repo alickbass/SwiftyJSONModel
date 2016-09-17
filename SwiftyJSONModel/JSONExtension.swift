b89df1a8f3ae2668024f0ddab8cfe49d6e218670
Oleksii Dykan <alick_bass@mail.ru>
2016-09-17 21:16:02 +0200

    Change the error in from element absent to invalid element

--- SwiftyJSONModel/JSONExtension.swift
+++ SwiftyJSONModel/JSONExtension.swift
@@ -11,44 +11,32 @@ import SwiftyJSON
 
 public extension JSON {
     public func boolValue() throws -> Bool {
-        guard let boolValue = bool else {
-            throw JSONModelError.elementAbsent
-        }
+        guard let boolValue = bool else { throw JSONModelError.invalidElement }
         return boolValue
     }
     
     public func intValue() throws -> Int {
-        guard let intValue = int else {
-            throw JSONModelError.elementAbsent
-        }
+        guard let intValue = int else { throw JSONModelError.invalidElement }
         return intValue
     }
     
     public func doubleValue() throws -> Double {
-        guard let doubleValue = double else {
-            throw JSONModelError.elementAbsent
-        }
+        guard let doubleValue = double else { throw JSONModelError.invalidElement }
         return doubleValue
     }
     
     public func stringValue() throws -> String {
-        guard let stringValue = string else {
-            throw JSONModelError.elementAbsent
-        }
+        guard let stringValue = string else { throw JSONModelError.invalidElement }
         return stringValue
     }
     
     public func arrayValue() throws -> [JSON] {
-        guard let arrayValue = array else {
-            throw JSONModelError.elementAbsent
-        }
+        guard let arrayValue = array else { throw JSONModelError.invalidElement }
         return arrayValue
     }
     
     public func uuidStringValue() throws -> String {
-        guard let stringValue = string else {
-            throw JSONModelError.elementAbsent
-        }
+        guard let stringValue = string else { throw JSONModelError.invalidElement }
         guard let uuidStrigValue = NSUUID(uuidString: stringValue) else {
             throw JSONModelError.invalidUUIDString
         }
