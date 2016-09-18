2fe2242da706fd57460b9b277389682e381224bc
Oleksii Dykan <alick_bass@mail.ru>
2016-09-18 23:29:31 +0200

    Add JSON Type file

---
+++ SwiftyJSONModel/JSONTypes.swift
@@ -0,0 +1,20 @@
+//
+//  JSONTypes.swift
+//  SwiftyJSONModel
+//
+//  Created by Oleksii on 18/09/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public protocol JSONInitializable {
+    init(json: JSON) throws
+}
+
+public protocol JSONRepresantable {
+    var jsonValue: JSON { get }
+}
+
+
