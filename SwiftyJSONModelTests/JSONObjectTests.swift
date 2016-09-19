95ee2027d15a1bccf98efb50409dbde3f5798604
Oleksii Dykan <alick_bass@mail.ru>
2016-09-20 00:36:09 +0200

    Rename json object type tests to json object tests

---
+++ SwiftyJSONModelTests/JSONObjectTests.swift
@@ -0,0 +1,15 @@
+//
+//  JSONObjectTypeTests.swift
+//  SwiftyJSONModel
+//
+//  Created by Oleksii on 19/09/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+import XCTest
+
+class JSONObjectTypeTests: XCTestCase {
+    
+    func testJSONObject()
+    
+}
