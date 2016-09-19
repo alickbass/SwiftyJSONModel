5cb9ff24bb39b13f855069fb5e8f529663d52756
Oleksii Dykan <alick_bass@mail.ru>
2016-09-19 20:09:11 +0200

    Rename json object type to json object file

---
+++ SwiftyJSONModel/JSONObject.swift
@@ -0,0 +1,29 @@
+//
+//  JSONObjectType.swift
+//  SwiftyJSONModel
+//
+//  Created by Oleksii on 19/09/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public struct JSONObject<PropertyType: RawRepresentable & Hashable>: JSONInitializable where PropertyType.RawValue == String {
+    private let json: JSON
+    
+    public init(json: JSON) throws {
+        guard json.type == .dictionary else {
+            throw JSONModelError.invalidJSON
+        }
+        self.json = json
+    }
+    
+    public func value<T: JSONInitializable>(for key: PropertyType) throws -> T {
+        return try T(json: json[key.rawValue])
+    }
+    
+    public func value<T: JSONInitializable>(for key: PropertyType) -> T? {
+        return try? value(for: key)
+    }
+}
--- SwiftyJSONModel/JSONObjectType.swift
+++
@@ -1,9 +0,0 @@
-//
-//  JSONObjectType.swift
-//  SwiftyJSONModel
-//
-//  Created by Oleksii on 19/09/16.
-//  Copyright © 2016 Oleksii Dykan. All rights reserved.
-//
-
-import Foundation
