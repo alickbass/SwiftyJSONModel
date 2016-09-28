678342cb24aeb310d576b2054593225b7fe5eb1a
Oleksii Dykan <alick_bass@mail.ru>
2016-09-28 17:48:33 +0200

    Cover invalid value for key error Equatable with tests

---
+++ SwiftyJSONModelTests/JSONModelErrorTests.swift
@@ -0,0 +1,24 @@
+//
+//  JSONModelErrorTests.swift
+//  SwiftyJSONModel
+//
+//  Created by Oleksii on 28/09/2016.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+import XCTest
+import SwiftyJSONModel
+
+class JSONModelErrorTests: XCTestCase {
+    
+    func testErrorEquatable() {
+        XCTAssertEqual(JSONModelError.invalidElement, .invalidElement)
+        XCTAssertEqual(JSONModelError.jsonIsNotAnObject, .jsonIsNotAnObject)
+        XCTAssertEqual(JSONModelError.invalidValueFor(key: "test", .invalidElement), .invalidValueFor(key: "test", .invalidElement))
+        
+        XCTAssertNotEqual(JSONModelError.invalidValueFor(key: "test", .invalidElement), .invalidValueFor(key: "other", .invalidElement))
+        XCTAssertNotEqual(JSONModelError.invalidValueFor(key: "test", .invalidElement), .invalidElement)
+        
+    }
+    
+}
