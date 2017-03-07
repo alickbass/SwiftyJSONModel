66b7db69368a488615ce81bf11e327dc68f531c3
Oleksii Dykan <alick_bass@mail.ru>
2017-03-07 10:53:16 +0100

    Add inits to JSON

--- SwiftyJSONModel/JSONExtension.swift
+++ SwiftyJSONModel/JSONExtension.swift
@@ -9,4 +9,11 @@
 import Foundation
 import SwiftyJSON
 
-extension JSON: JSONType {}
+extension JSON: JSONType {
+    public init(bool: Bool) { self.init(bool) }
+    public init(int: Int) { self.init(int) }
+    public init(double: Double) { self.init(double) }
+    public init(string: String) { self.init(string) }
+    public init(array: [JSON]) { self.init(array) }
+    public init(dictionary: [String: JSON]) { self.init(dictionary) }
+}
