814a7e3f4b42e73c99ad4418872ae4a561dbcaf6
Oleksii Dykan <alick_bass@mail.ru>
2017-03-07 10:19:16 +0100

    Make Swifty JSON’s JSON conform to JSONType Protocol

--- SwiftyJSONModel/JSONExtension.swift
+++ SwiftyJSONModel/JSONExtension.swift
@@ -9,34 +9,4 @@
 import Foundation
 import SwiftyJSON
 
-public extension JSON {
-    public func value() throws -> Bool {
-        guard let boolValue = bool else { throw JSONModelError.invalidElement }
-        return boolValue
-    }
-    
-    public func value() throws -> Int {
-        guard let intValue = int else { throw JSONModelError.invalidElement }
-        return intValue
-    }
-    
-    public func value() throws -> Double {
-        guard let doubleValue = double else { throw JSONModelError.invalidElement }
-        return doubleValue
-    }
-    
-    public func value() throws -> String {
-        guard let stringValue = string else { throw JSONModelError.invalidElement }
-        return stringValue
-    }
-    
-    public func arrayValue() throws -> [JSON] {
-        guard let arrayValue = array else { throw JSONModelError.invalidElement }
-        return arrayValue
-    }
-    
-    public func dictionaryValue() throws -> [String: JSON] {
-        guard let dictValue = dictionary else { throw JSONModelError.invalidElement }
-        return dictValue
-    }
-}
+extension JSON: JSONType {}
